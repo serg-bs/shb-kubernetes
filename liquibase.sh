750140b466a4dec1a95a481fd3bc37f6fbbeb905
Sergey Bespalov <bespalov@srvhub.ru>
2018-11-30 17:38:14 +0300

    change Readme + clear kubectl yaml

--- liquibase.sh
+++
@@ -1,9 +0,0 @@
-docker run -it \
---name liquibase \
---link postgres: \
---entrypoint="/scripts/liquibase_command.sh" \
--v /$LIQUIBASE_CHANGELOGS:/changelogs \
--e CONNECTION_STRING="jdbc:postgresql://$DB_IP:5432/$DB_NAME" \
--e CHANGELOG_FILE=/home/serg/IdeaProjects/shb/builds/db/src/main/changelogs/master.xml \
-sequenceiq/docker-liquibase\
-"update"
